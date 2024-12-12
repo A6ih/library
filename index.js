920a5bf23bc30a077e9402d91ed18bd4435e602b
A6ih <177154114+A6ih@users.noreply.github.com>
2024-12-12 17:50:44 +0200

    Add function to display book cards

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages) {
+    library.push(new Book(title, author, pages))
 }
+
+const cardContainer = document.querySelector("#cards-container");
+
+function displayBooks(){
+   return library.map(book => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "book-card");
+        const title = document.createElement("h4");
+        title.setAttribute("class"," title");
+        const author = document.createElement("p");
+        author.setAttribute("class", "author")
+        const pages = document.createElement("p");
+        author.setAttribute("class", "no-of-pages")
+        title.textContent = `Title: ${book.title}`;
+        author.textContent = `Author: ${book.author}`;
+        pages.textContent = `No of pages: ${book.pages}`;
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        cardContainer.appendChild(card);
+    })
+}
+
+
+addBookToLibrary("Book", "Abhi", 5);
+addBookToLibrary("Book2", "Abhishek", 15);
+addBookToLibrary("Book3", "Abhish", 25);
+console.log(library)
+
+displayBooks();
