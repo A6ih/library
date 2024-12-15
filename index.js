d5afe4a3936d3f368271375fc9b13f30b461b38a
A6ih <177154114+A6ih@users.noreply.github.com>
2024-12-15 18:35:05 +0200

    Add remove book button and function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const library = [];
+let counter = 1;
 
 function Book(title, author, pages) {
+    this.id = `b${counter++}`;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -17,26 +19,32 @@ function displayBooks(){
         const card = document.createElement("div");
         card.setAttribute("class", "book-card");
         const title = document.createElement("h4");
-        title.setAttribute("class"," title");
+        title.setAttribute("class", "title");
         const author = document.createElement("p");
         author.setAttribute("class", "author")
         const pages = document.createElement("p");
         author.setAttribute("class", "no-of-pages")
+        const removeBtn = document.createElement("button");
+        removeBtn.setAttribute("class", "remove-btn");
+        removeBtn.setAttribute("id", book.id)
+        removeBtn.textContent = "-";
         title.textContent = `Title: ${book.title}`;
         author.textContent = `Author: ${book.author}`;
-        pages.textContent = `No of pages: ${book.pages}`;
+        pages.textContent = `No of pages: ${book.pages} pages`;
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(removeBtn);
         cardContainer.appendChild(card);
+        removeBtn.addEventListener("click", removeBook)
     })
 }
 
 
-addBookToLibrary("Book", "Abhi", 5);
-addBookToLibrary("Book2", "Abhishek", 15);
-addBookToLibrary("Book3", "Abhish", 25);
-console.log(library)
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
+addBookToLibrary("Harry Potter and the Chamber of Secrets", "J.K Rowling", 251);
+addBookToLibrary("A Tale of Two Cities", "Charles Dickens", 300);
+
 
 displayBooks();
 
@@ -63,4 +71,18 @@ addBookBtn.addEventListener("click", event => {
     addBookToLibrary(title, author, pages);
     cardContainer.textContent = "";
     displayBooks();
+    console.log(library);
 })
+
+
+function removeBook(event) {
+    library.forEach(book => {
+        if(event.target.getAttribute("id") === book.id) {
+            let index = library.indexOf(book);
+            library.splice(index, 1)
+        }
+    })
+    console.log(library);
+    cardContainer.textContent = "";
+    displayBooks();
+}
